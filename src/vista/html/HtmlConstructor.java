ee4a66fa5e07d2868cd04f9a925d07fa2271db0a
Tofol <tofolcanodaw2@gmail.com>
2019-11-13 17:54:56 +0100

    encarrilando las páginas específicas

--- src/vista/html/HtmlConstructor.java
+++ src/vista/html/HtmlConstructor.java
@@ -1,5 +1,25 @@
 package vista.html;
 
 public class HtmlConstructor {
+	
+	private String head = "";
+	private String abreBody = "";
+	private String abreHeader = "";
+	private String contenidoHeader = "";
+	private String cierraHeader = "";
+	private String contenidoBody = "";
+	private String abreFooter = "";
+	private String contenidoFooter = "";
+	private String cierraFooter = "";
+	private String cierraBody = "";
+
+	/****
+	 * escribe lo que todas las páginas tienen en común
+	 */
+	public HtmlConstructor() {
+		this.abreBody = "<body>";
+		this.cierraBody = "</body>";
+		//etc.
+	}
 
 }
--- src/vista/html/MainPage.java
+++ src/vista/html/MainPage.java
@@ -2,4 +2,17 @@ package vista.html;
 
 public class MainPage extends HtmlConstructor {
 
+	private String abreNavBar = "";
+	private String contenidoNavBar = "";
+	private String cierraNavBar = "";
+	private String buscador = "";
+	
+	/***
+	 * Añade código html específico de esta página
+	 * @param usuario Si hay usuario lo añade en el contenido header que hereda de la clase padre
+	 * @param esAdmin Si es true añade el link al servlet AddJuego en el
+	 */
+	public MainPage(String usuario, boolean esAdmin) {
+		super();
+	}
 }
